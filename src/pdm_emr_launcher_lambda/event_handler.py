3b124d22aafdb0d7c9fdf64053e65bb2121cbdbd
Sandeep Gopishetty <sandeep.gopishetty@dmwgroup.co.uk>
2021-03-04 16:41:20 +0000

    DW-5721 added SNS arg

---
+++ src/pdm_emr_launcher_lambda/event_handler.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+
+"""pdm_emr_launcher_lambda"""
+import argparse
+import json
+import logging
+import os
+import socket
+import sys
+from datetime import datetime as date_time
+
+import boto3
+from boto3.dynamodb.conditions import Attr
+
+DATA_PRODUCT_KEY = "DataProduct"
+STATUS_KEY = "Status"
+STATUS_COMPLETED = "Completed"
+DATE_KEY = "Date"
+DATA_PRODUCT_NAME = "ADG-full"
+SNS_TOPIC = "adg_completion_status_sns"
+
+args = None
+logger = None
+
+
+def handler(event, context):
+    """Handle the event from AWS.
+
+    Args:
+        event (Object): The event details from AWS
+        context (Object): The context info from AWS
+
+    """
+    global logger
+    logger = setup_logging("INFO")
+    logger.info(f'Cloudwatch Event": {event}')
+    try:
+        logger.info(os.getcwd())
+        handle_event()
+    except Exception as err:
+        logger.error(f'Exception occurred for invocation", "error_message": {err}')
+
+
+def handle_event():
+    """
+    Reads relevant record from dynamoDB table and sens SNS message
+    """
+    global args
+
+    args = get_environment_variables()
+
+    if not args.sns_topic:
+        raise Exception("Required environment variable SNS_TOPIC is unset")
+
+    if not args.table_name:
+        raise Exception("Required environment variable TABLE_NAME is unset")
+
+    sns_client = get_sns_client()
+    today = get_todays_date()
+    dynamo_client = get_dynamo_table(args.table_name)
+    dynamo_items = query_dynamo(dynamo_client, today)
+
+    if not dynamo_items:
+        logger.info(
+            f"No item found in DynamoDb table {args.table_name} for date {today} and DataProduct {DATA_PRODUCT_NAME}"
+        )
+    else:
+        logger.info(f"Sending message to SNS topic to launch PDM cluster")
+
+        payload = generate_lambda_launcher_payload(dynamo_items[0])
+
+        sns_response = send_sns_message(sns_client, payload, args.sns_topic)
+        logger.info(f"Response from Sns: {sns_response}.")
+
+
+def get_sns_client():
+    return boto3.client("sns")
+
+
+def send_sns_message(sns_client, payload, sns_topic_arn):
+    """Publishes the message to adg_completion_status_sns SNS topic.
+
+    Arguments:
+        sns_client (client): The boto3 client for SNS
+        payload (dict): the payload to post to SNS
+        sns_topic_arn (string): the arn for the SNS topic
+
+    """
+    dumped_payload = get_escaped_json_string(payload)
+    logger.info(
+        f'Publishing payload to SNS", "payload": {dumped_payload}, "sns_topic_arn": "{sns_topic_arn}"'
+    )
+
+    return sns_client.publish(TopicArn=sns_topic_arn, Message=dumped_payload)
+
+
+def query_dynamo(dynamo_table, today):
+    """Queries the DynamoDb table for todays run of ADG-full application, we will just use Run_Id as 1
+
+    Arguments:
+        dynamo_table (table): The boto3 table for DynamoDb
+        today: Today's date
+
+    Returns:
+        list: The items matching the scan operation
+
+    """
+    response = dynamo_table.scan(
+        FilterExpression=Attr(DATE_KEY).eq(today)
+        & Attr(DATA_PRODUCT_KEY).eq(DATA_PRODUCT_NAME)
+        & Attr(STATUS_KEY).eq(STATUS_COMPLETED)
+    )
+    logger.info(f"Response from dynamo {response}")
+    return response["Items"]
+
+
+def get_todays_date():
+    return date_time.now().strftime("%Y-%m-%d")
+
+
+def generate_lambda_launcher_payload(dynamo_item):
+    payload = {
+        "correlation_id": dynamo_item["Correlation_Id"],
+        "s3_prefix": dynamo_item["S3_Prefix_Analytical_DataSet"],
+    }
+    logger.info(f"Lambda payload: {payload}")
+    return payload
+
+
+def setup_logging(logger_level):
+    """Set the default logger with json output."""
+    the_logger = logging.getLogger()
+    for old_handler in the_logger.handlers:
+        the_logger.removeHandler(old_handler)
+
+    new_handler = logging.StreamHandler(sys.stdout)
+    hostname = socket.gethostname()
+
+    json_format = (
+        f'{{ "timestamp": "%(asctime)s", "log_level": "%(levelname)s", "message": "%(message)s", '
+        f'"module": "%(module)s", "process":"%(process)s", '
+        f'"thread": "[%(thread)s]", "host": "{hostname}" }}'
+    )
+
+    new_handler.setFormatter(logging.Formatter(json_format))
+    the_logger.addHandler(new_handler)
+    new_level = logging.getLevelName(logger_level)
+    the_logger.setLevel(new_level)
+
+    if the_logger.isEnabledFor(logging.DEBUG):
+        boto3.set_stream_logger()
+        the_logger.debug(f'Using boto3", "version": "{boto3.__version__}')
+
+    return the_logger
+
+
+def get_environment_variables():
+    """Retrieve the required environment variables.
+
+    Returns:
+        args: The parsed and validated environment variables
+
+    """
+    parser = argparse.ArgumentParser()
+
+    _args = parser.parse_args()
+
+    if "SNS_TOPIC" in os.environ:
+        _args.sns_topic = os.environ["SNS_TOPIC"]
+
+    if "TABLE_NAME" in os.environ:
+        _args.table_name = os.environ["TABLE_NAME"]
+
+    if "LOG_LEVEL" in os.environ:
+        _args.log_level = os.environ["LOG_LEVEL"]
+
+    return _args
+
+
+def get_dynamo_table(table_name):
+    """Retrieve the boto3 DynamoDb Table resource.
+
+    Returns:
+        table: A resource representing an Amazon DynamoDB Table
+
+    """
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(table_name)
+    return table
+
+
+def get_escaped_json_string(json_string):
+    escaped_string = json.dumps(json_string)
+    logger.info(f"Escaped json: {json_string}")
+    return escaped_string
---
+++ tests/test_pdm_emr_launcher_handler.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+
+"""pdm_emr_launcher_lambda"""
+import argparse
+import unittest
+from unittest import mock
+
+import boto3
+from datetime import datetime
+from moto import mock_dynamodb2
+from pdm_emr_launcher_lambda import event_handler
+
+TEST_DATE = "2000-10-01"
+SNS_TOPIC_ARN = "test-sns-topic-arn"
+TABLE_NAME = "data_pipeline_metadata"
+
+args = argparse.Namespace()
+args.sns_topic = SNS_TOPIC_ARN
+args.table_name = TABLE_NAME
+args.log_level = "INFO"
+
+
+class TestPDMLauncher(unittest.TestCase):
+    @mock_dynamodb2
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.logger")
+    def test_query_dynamo_item_exists(self, mock_logger):
+        dynamodb_resource = self.mock_get_dynamodb_table(self.get_todays_date())
+        result = event_handler.query_dynamo(dynamodb_resource, self.get_todays_date())
+        self.assertEqual(
+            result,
+            [
+                {
+                    "Correlation_Id": "test_correlation_id",
+                    "DataProduct": "ADG-full",
+                    "Date": self.get_todays_date(),
+                    "S3_Prefix_Analytical_DataSet": "test_s3_prefix",
+                    "Status": "Completed",
+                }
+            ],
+        )
+
+    @mock_dynamodb2
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.logger")
+    def test_query_dynamo_item_empty_result(self, mock_logger):
+        dynamodb_resource = self.mock_get_dynamodb_table(TEST_DATE)
+        result = event_handler.query_dynamo(dynamodb_resource, "test")
+        self.assertEqual(result, [])
+
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.send_sns_message")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.setup_logging")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.get_environment_variables")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.get_dynamo_table")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.get_sns_client")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.logger")
+    @mock_dynamodb2
+    def test_handler_sns_message_sent(
+        self,
+        mock_logger,
+        get_sns_client_mock,
+        get_dynamo_table_mock,
+        get_environment_variables_mock,
+        setup_logging_mock,
+        send_sns_message_mock,
+    ):
+        dynamodb_resource = self.mock_get_dynamodb_table(self.get_todays_date())
+        get_dynamo_table_mock.return_value = dynamodb_resource
+
+        sns_client_mock = mock.MagicMock()
+        get_sns_client_mock.return_value = sns_client_mock
+        get_environment_variables_mock.return_value = args
+
+        event_handler.handler(event={}, context=None)
+
+        send_sns_message_mock.assert_called_once_with(
+            sns_client_mock,
+            {"correlation_id": "test_correlation_id", "s3_prefix": "test_s3_prefix"},
+            args.sns_topic,
+        )
+
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.send_sns_message")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.setup_logging")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.get_environment_variables")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.get_dynamo_table")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.get_sns_client")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.logger")
+    @mock_dynamodb2
+    def test_handler_sns_message_not_sent_when_no_items_in_dynamo(
+        self,
+        mock_logger,
+        get_sns_client_mock,
+        get_dynamo_table_mock,
+        get_environment_variables_mock,
+        setup_logging_mock,
+        send_sns_message_mock,
+    ):
+        dynamodb_resource = self.mock_get_dynamodb_table(TEST_DATE)
+        get_dynamo_table_mock.return_value = dynamodb_resource
+
+        sns_client_mock = mock.MagicMock()
+        get_sns_client_mock.return_value = sns_client_mock
+        get_environment_variables_mock.return_value = args
+
+        event_handler.handler(event={}, context=None)
+
+        send_sns_message_mock.assert_not_called()
+
+    def get_todays_date(self):
+        return datetime.now().strftime("%Y-%m-%d")
+
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.setup_logging")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.get_environment_variables")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.get_sns_client")
+    @mock.patch("pdm_emr_launcher_lambda.event_handler.logger")
+    def test_handler_invalid_environment_variable(
+        self,
+        mock_logger,
+        get_sns_client_mock,
+        get_environment_variables_mock,
+        setup_logging_mock,
+    ):
+        sns_client_mock = mock.MagicMock()
+        get_sns_client_mock.return_value = sns_client_mock
+        get_environment_variables_mock.return_value = argparse.Namespace()
+
+        with self.assertRaises(Exception) as context:
+            event_handler.handle_event()
+
+    @mock_dynamodb2
+    def mock_get_dynamodb_table(self, date):
+        dynamodb_client = boto3.client("dynamodb", region_name="eu-west-2")
+        dynamodb_client.create_table(
+            TableName=TABLE_NAME,
+            KeySchema=[
+                {"AttributeName": "Correlation_Id", "KeyType": "HASH"},  # Partition key
+                {"AttributeName": "DataProduct", "KeyType": "RANGE"},  # Sort key
+            ],
+            AttributeDefinitions=[
+                {"AttributeName": "Correlation_Id", "AttributeType": "S"},
+                {"AttributeName": "DataProduct", "AttributeType": "S"},
+            ],
+            ProvisionedThroughput={"ReadCapacityUnits": 10, "WriteCapacityUnits": 10},
+        )
+        dynamodb = boto3.resource("dynamodb", region_name="eu-west-2")
+        table = dynamodb.Table(TABLE_NAME)
+
+        item = {
+            "Correlation_Id": "test_correlation_id",
+            "DataProduct": "ADG-full",
+            "Date": date,
+            "S3_Prefix_Analytical_DataSet": "test_s3_prefix",
+            "Status": "Completed",
+        }
+
+        table.put_item(TableName=TABLE_NAME, Item=item)
+
+        return table
