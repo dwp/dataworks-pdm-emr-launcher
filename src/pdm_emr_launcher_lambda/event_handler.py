54a402528970c534e8a7d2356dab7682ddc8295f
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-10-11 16:56:25 +0000

    Fixup: Format Python code with Black

--- src/pdm_emr_launcher_lambda/event_handler.py
+++ src/pdm_emr_launcher_lambda/event_handler.py
@@ -35,8 +35,6 @@ def handler(event, context):
     global logger
     logger = setup_logging("INFO")
 
-
-
     logger.info(f'Cloudwatch Event": {event}')
     try:
         logger.info(os.getcwd())
